dc3df318a25493228738c12cc705433eb39c93e0
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-30 02:10:01 +0300

    Add tests for forceDeque and forceOnTimeDeque

--- src/test/java/deferred_queue/core/DeferredQueueTest.java
+++ src/test/java/deferred_queue/core/DeferredQueueTest.java
@@ -1,7 +1,11 @@
 package deferred_queue.core;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
+import static deferred_queue.core.Delay.delay;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -10,7 +14,12 @@ import static org.junit.Assert.assertEquals;
  */
 public class DeferredQueueTest {
 
-    private DeferredQueue<Integer> queue = new DeferredQueue<>();
+    private DeferredQueue<Integer> queue;
+
+    @Before
+    public void setUp() throws Exception {
+        queue = new DeferredQueue<>();
+    }
 
     @Test
     public void forceDequeTest() throws Exception {
@@ -23,8 +32,40 @@ public class DeferredQueueTest {
                 assertEquals(100500, ((long) value));
             }
         });
-        queue.insert(100500, null);
-        queue.insert(666, null);
+        queue.insert(100500, delay(TimeUnit.HOURS, 1));
+        queue.insert(666, delay(TimeUnit.HOURS, 1));
+        assertEquals(true, callbackExecuted[0]);
+    }
+
+    @Test
+    public void forceOnTimePullTest() throws Exception {
+        final boolean[] callbackExecuted = {false};
+
+        queue.updateOnTimeDequeCallback(new Callback<Integer>() {
+            @Override
+            public void call(Integer value) {
+                callbackExecuted[0] = true;
+                assertEquals(100500, ((long) value));
+            }
+        });
+        queue.insert(100500, delay(TimeUnit.HOURS, 1));
+        queue.forceOnTimePull();
+        assertEquals(true, callbackExecuted[0]);
+    }
+
+    @Test
+    public void forcePullTest() throws Exception {
+        final boolean[] callbackExecuted = {false};
+
+        queue.updateOnForceDequeCallback(new Callback<Integer>() {
+            @Override
+            public void call(Integer value) {
+                callbackExecuted[0] = true;
+                assertEquals(100500, ((long) value));
+            }
+        });
+        queue.insert(100500, delay(TimeUnit.HOURS, 1));
+        queue.forcePull();
         assertEquals(true, callbackExecuted[0]);
     }
 }
