25e7a64c118b92a3fb846a44811e12e92eb33896
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-05-01 02:49:23 +0300

    Add tests for queue

--- src/test/java/deferred_queue/core/DeferredQueueTest.java
+++ src/test/java/deferred_queue/core/DeferredQueueTest.java
@@ -3,6 +3,7 @@ package deferred_queue.core;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static deferred_queue.core.Delay.delay;
@@ -82,6 +83,57 @@ public class DeferredQueueTest {
         queue.insert(100500, delay(TimeUnit.SECONDS, 1));
         threadWait(delay(TimeUnit.SECONDS, 2));
         assertEquals(true, callbackExecuted[0]);
+    }
+
+    @Test
+    public void twoTaskSubmittedWithDelayTest() throws Exception {
+        final int[] callbackExecuted = {0};
+
+        queue.updateOnTimeDequeCallback(new Callback<Integer>() {
+            @Override
+            public void call(Integer value) {
+                if (callbackExecuted[0] == 0) {
+                    assertEquals(666, ((long) value));
+                }
+                if (callbackExecuted[0] == 1) {
+                    assertEquals(777, ((long) value));
+                }
+                callbackExecuted[0] += 1;
+
+            }
+        });
+        queue.insert(666, delay(TimeUnit.MILLISECONDS, 500));
+        threadWait(delay(TimeUnit.SECONDS, 2));
+        queue.insert(777, delay(TimeUnit.MILLISECONDS, 500));
+        threadWait(delay(TimeUnit.SECONDS, 2));
+        assertEquals(2, callbackExecuted[0]);
+    }
+
+    @Test
+    public void twoTaskSortedTest() throws Exception {
+        queue = new DeferredQueue<>(2, Executors.newSingleThreadExecutor());
+
+        final int[] callbackExecuted = {0};
+
+        queue.updateOnTimeDequeCallback(new Callback<Integer>() {
+            @Override
+            public void call(Integer value) {
+                if (callbackExecuted[0] == 0) {
+                    assertEquals(777, ((long) value));
+                }
+                if (callbackExecuted[0] == 1) {
+                    assertEquals(666, ((long) value));
+                }
+                callbackExecuted[0] += 1;
+
+            }
+        });
+        queue.insert(666, delay(TimeUnit.MILLISECONDS, 500));
+        // second insert must appear earlier then first
+        queue.insert(777, delay(TimeUnit.MILLISECONDS, 100));
+        threadWait(delay(TimeUnit.SECONDS, 2));
+        assertEquals(2, callbackExecuted[0]);
+
     }
 
     private void threadWait(Delay delay) {
