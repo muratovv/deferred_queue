de2abcf1a757390c530ec3e6861708a46ddb733c
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-30 03:27:13 +0300

    Add to millis method; small refactoring

--- src/main/java/deferred_queue/core/Delay.java
+++ src/main/java/deferred_queue/core/Delay.java
@@ -3,18 +3,22 @@ package deferred_queue.core;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Class represents waiting time for pulled from queue
+ * Class represents waiting duration for pulled from queue
  */
 public class Delay {
     private TimeUnit unit;
-    private long     time;
+    private long     duration;
 
-    private Delay(TimeUnit unit, long time) {
+    private Delay(TimeUnit unit, long duration) {
         this.unit = unit;
-        this.time = time;
+        this.duration = duration;
     }
 
-    public static Delay delay(TimeUnit unit, long time) {
-        return new Delay(unit, time);
+    public long toMillis() {
+        return TimeUnit.MILLISECONDS.convert(duration, unit);
+    }
+
+    public static Delay delay(TimeUnit unit, long duration) {
+        return new Delay(unit, duration);
     }
 }
